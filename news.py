f11cebbb116ee7d7680e86c5c4a3030338e50296
Jatin-codes-git <jatin.codes@outlook.com>
2025-03-03 17:28:06 +0530

    Update news.py

--- news.py
+++ news.py
@@ -10,10 +10,9 @@ import os
 
 # Setup NLTK 'punkt' tokenizer
 nltk_data_path = os.path.join(os.getcwd(), "nltk_data")
-if not os.path.exists(nltk_data_path):
-    os.makedirs(nltk_data_path)
 nltk.download('punkt', download_dir=nltk_data_path)
 nltk.data.path.append(nltk_data_path)
+
 
 
 # Function to extract article text using BeautifulSoup
